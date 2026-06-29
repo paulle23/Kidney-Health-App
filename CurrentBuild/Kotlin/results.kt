0fa5081d6410ea91ade2df30469fd824199ab34e
Paul Le <50690282+paulle23@users.noreply.github.com>
2026-06-28 23:55:27 -0700

    Refactor results.kt with improved comments
    Updated comments for clarity and added explanatory notes.

--- CurrentBuild/Kotlin/results.kt
+++ CurrentBuild/Kotlin/results.kt
@@ -5,22 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firedatabase_assis.databinding.ActivityResultsBinding
 
-
-//readings page
+/*results page where user has their information
+displayed and can choose to go to camera, gallery, or logout*/
 class results : AppCompatActivity() {
-    private lateinit var bind: ActivityResultsBinding
+    private lateinit var bind: ActivityResultsBinding//binding object to access ui elements without findViewbyID
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bind= ActivityResultsBinding.inflate(layoutInflater)
-        setContentView(bind.root)
+        bind= ActivityResultsBinding.inflate(layoutInflater)//inflates layout 
+        setContentView(bind.root)//shows layout on screen
+        //Get all the user information
         bind.uname.text = UserSession.username
         bind.password.text = UserSession.password//value1 (optional: avoid storing passwords globally)
         bind.name.text = UserSession.name
         bind.demographic.text = UserSession.demographic
         bind.email.text = UserSession.email
-        bind.logout.setOnClickListener {
-            UserSession.clear()
+        bind.logout.setOnClickListener {//logout button
+            UserSession.clear()//clear the session 
             val intent = Intent(this, login_form::class.java)
+            //remove all previous activities from memory and prevents user from pressing back to return after logout
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
