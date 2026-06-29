7c7c68f6c9d1f52d797467ebc42ec32dbffd204f
Paul Le <50690282+paulle23@users.noreply.github.com>
2026-06-28 23:45:48 -0700

    Refactor UserSession.kt with improved comments
    Added comments for clarity on UserSession object.

--- CurrentBuild/Kotlin/UserSession.kt
+++ CurrentBuild/Kotlin/UserSession.kt
@@ -1,8 +1,8 @@
 package com.example.firedatabase_assis
 
 //stores the userSession so that other activities can access it without needing to pass intent
-object UserSession {
-    var name: String? = null
+object UserSession {//object so that there is only one instance throughout app
+    var name: String? = null//String? means it is nullable
     var username: String? = null
     var password: String? = null
     var email: String? = null
